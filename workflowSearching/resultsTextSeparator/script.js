c948be676792642021951986ec45ddfec5311fc8
Ranjan <Prabhat.P.Ranjan2@shell.com>
2022-10-31 18:10:31 +0530

    resultTextSeparator added

---
+++ workflowSearching/resultsTextSeparator/script.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const data = require('./data.json');
+
+// To get all the values
+function resultFilter(data) {
+    return data.reduce((ac, cr) => {
+        cr?.innerTabs?.forEach(item => {
+            item?.content?.forEach(it => {
+                if (it.type === 'resultbox') {
+                    ac.push(it.label);
+                }
+            })
+        })
+        return ac;
+    }, []);
+}
+
+fs.writeFile('result.json', JSON.stringify(resultFilter(data)), (err, data) => {
+    if (err) {
+        throw Error('Write failed!');
+    }
+})
